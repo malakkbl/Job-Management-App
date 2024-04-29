328f6dc1aefce3dc99d1368b834a9fe363bc4db7
unknown <malak012426@gmail.com>
2024-04-29 12:22:33 +0000

    delete job done -- still need : use the fetch thing + css

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -6,9 +6,9 @@
       <li v-for="job in jobs" :key="job.id">
         <h3>{{ job.titre }}</h3>
         <button @click="viewJobDetail(job.id)">View Details</button>
+        <button @click="deleteJob(job.id)">Delete</button>
       </li>
     </ul>
-    
   </div>
 </template>
 
@@ -28,6 +28,12 @@ export default {
     },
     goToAddJob() {
       this.$router.push({ name: 'AddJob' });
+    },
+    deleteJob(id) {
+      const index = this.jobs.findIndex(job => job.id === id);
+      if (index !== -1) {
+        this.jobs.splice(index, 1);
+      }
     }
   }
 };
